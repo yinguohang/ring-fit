c9e20d859db0e7a24c0be236304210fe35266d68
yinguohang <yinguohang@gmail.com>
2020-05-17 03:43:39 -0700

    Add select for ingredient detail

--- src/views/IngredientPage.vue
+++ src/views/IngredientPage.vue
@@ -1,12 +1,20 @@
 <template>
   <el-container>
     <el-container>
-      <el-aside width="100px" style="height: calc(100vh - 85px);">
+      <el-aside class='sidebar' width="100px" style="height: calc(100vh - 85px);">
         <el-scrollbar style="height: 100%; overflow-x: hidden">
           <ingredient-list></ingredient-list>
         </el-scrollbar>
       </el-aside>
       <el-main>
+        <el-select v-model="selectedIngredient" filterable>
+          <el-option v-for="ingredient in ingredients"
+                     :key="ingredient[0]"
+                     :label="ingredient[1]"
+                     :value="ingredient[0]">
+            {{ingredient[1]}}
+          </el-option>
+        </el-select>
         <ingredient-detail></ingredient-detail>
       </el-main>
     </el-container>
@@ -23,12 +31,27 @@ export default {
     '$route' () {
       this.$store.commit('updateSelectedIngredient', this.$route.params.id)
     }
+  },
+  computed: {
+    ingredients () {
+      return this.$store.state.ingredients.map(
+        x => ([x[0], this.$t('ingredients[' + this.$store.state.ingredientEnToID[x[1]] + ']')])
+      )
+    },
+    selectedIngredient: {
+      get () {
+        return this.$route.params.id
+      },
+      set (newVal) {
+        this.$store.commit('updateSelectedIngredient', newVal)
+      }
+    }
   }
 }
 </script>
 
 <style>
-.el-scrollbar__wrap {
+.sidebar .el-scrollbar__wrap {
   overflow-x:hidden !important;
 }
 </style>
